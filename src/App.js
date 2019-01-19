f38a7e9e8020e65a72c52e1b6db4fbe86f28fbc3
James Priest <james-priest@outlook.com>
2019-01-19 05:53:20 -0800

    refactor: Moved contacts into App component state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    id: 'karen',
-    name: 'Karen Isgrigg',
-    handle: 'karen_isgrigg',
-    avatarURL: './avatars/karen.jpg'
-  },
-  {
-    id: 'richard',
-    name: 'Richard Kalehoff',
-    handle: 'richardkalehoff',
-    avatarURL: './avatars/richard.jpg'
-  },
-  {
-    id: 'tyler',
-    name: 'Tyler McGinnis',
-    handle: 'tylermcginnis',
-    avatarURL: './avatars/tyler.jpg'
-  }
-];
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: 'karen_isgrigg',
+        avatarURL: './avatars/karen.jpg'
+      },
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: 'richardkalehoff',
+        avatarURL: './avatars/richard.jpg'
+      },
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: 'tylermcginnis',
+        avatarURL: './avatars/tyler.jpg'
+      }
+    ]
+  };
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
