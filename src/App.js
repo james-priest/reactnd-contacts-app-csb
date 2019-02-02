03acfefd11c28db6b3afc4912197302fb68e23e5
James Priest <james-priest@outlook.com>
2019-02-01 21:10:24 -0800

    feat: Show CreateContact based on state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import ListContacts from "./ListContacts";
 import * as ContactsAPI from "./utils/ContactsAPI";
+import CreateContact from "./CreateContact";
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    screen: "create"
   };
   componentDidMount = () => {
     ContactsAPI.getAll().then(contacts => {
@@ -12,10 +14,6 @@ class App extends Component {
     });
   };
   removeContact = contact => {
-    // this.setState(currentState => ({
-    //   contacts: currentState.contacts.filter(c => c.id !== contact.id)
-    // }));
-    // ContactsAPI.remove(contact);
     ContactsAPI.remove(contact)
       .then(contact => {
         this.setState(prevState => ({
@@ -27,10 +25,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
+        {this.state.screen === "list" && (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )}
+        {this.state.screen === "create" && <CreateContact />}
       </div>
     );
   }
